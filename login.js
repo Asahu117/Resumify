2a01759d8810afaa80652c67ef209660760cbd01
Asahu117 <112420597+Asahu117@users.noreply.github.com>
2023-09-10 20:19:56 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,42 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const fastn = require('fastn')({
-        // Add any custom settings here
-    });
+    const loginLink = document.getElementById("login-link");
+    const loginContainer = document.getElementById("login-container");
+    const loginForm = document.getElementById("login-form");
+    const loginMessage = document.getElementById("login-message");
 
-    const loginLink = fastn('a', { href: '#', id: 'login-link' }, 'Login');
-    const loginContainer = fastn('div', { id: 'login-container' });
-    const loginForm = fastn('form', { id: 'login-form' });
-    const loginMessage = fastn('div', { id: 'login-message' });
-
-    loginLink.on('click', function (event) {
-        event.preventDefault();
+    loginLink.addEventListener("click", function (e) {
+        e.preventDefault();
         toggleLogin();
     });
 
-    loginForm.on('submit', function (event) {
-        event.preventDefault();
+    loginForm.addEventListener("submit", function (e) {
+        e.preventDefault();
         const username = document.getElementById("username").value;
         const password = document.getElementById("password").value;
 
         // Replace this with your authentication logic
         if (username === "user" && password === "password") {
-            loginMessage.text('Login successful!').style({ color: 'green' });
+            loginMessage.textContent = "Login successful!";
+            loginMessage.style.color = "green";
         } else {
-            loginMessage.text('Invalid username or password.').style({ color: 'red' });
+            loginMessage.textContent = "Invalid username or password.";
+            loginMessage.style.color = "red";
         }
     });
 
     function toggleLogin() {
-        if (!loginContainer.element.parentNode) {
-            fastn('body').appendChild(loginContainer);
+        if (loginContainer.style.display === "none" || loginContainer.style.display === "") {
+            loginContainer.style.display = "block";
         } else {
-            loginContainer.detach();
-            loginMessage.text(''); // Clear any previous login message
+            loginContainer.style.display = "none";
+            loginMessage.textContent = ""; // Clear any previous login message
         }
     }
-
-    fastn('body', loginLink, loginContainer);
-    fastn(loginContainer, loginForm, loginMessage);
-
-    toggleLogin(); // Initially hide the login form
 });
